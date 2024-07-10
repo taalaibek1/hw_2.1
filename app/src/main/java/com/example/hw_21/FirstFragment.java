94b0290e4c04981408ba28d51d4b65a131f9c6c0
taalaibek1 <87862312+taalaibek1@users.noreply.github.com>
2024-07-10 19:05:47 +0600

    Update FirstFragment.java

--- app/src/main/java/com/example/hw_21/FirstFragment.java
+++ app/src/main/java/com/example/hw_21/FirstFragment.java
@@ -18,7 +18,6 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_first, container, false);
 
         textViewCounter = view.findViewById(R.id.textViewCounter);
